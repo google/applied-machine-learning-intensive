af95f33cfa07856f2100b40068636da546d9e95e
Josh McAdams <joshua.mcadams@gmail.com>
2019-12-17 16:04:09 -0800

    Adding data for binary classification

--- amli/v2/content/04_binary_classification/02_binary_classification/src/gen_data.py
+++ amli/v2/content/04_binary_classification/02_binary_classification/src/gen_data.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-FRUIT_COUNT = 10000
+FRUIT_COUNT = 1000
 
 ORANGE = {
   'diameter': {
     'min': 6,
     'max': 10,
-    'avg': (10 - 6) / 2,
+    'avg': (10 + 6) / 2,
   },
   'weight': { # grams
     'min': 96,
@@ -24,7 +24,7 @@ GRAPEFRUIT = {
   'diameter': {
     'min': 10,
     'max': 15,
-    'avg': (15 - 10) / 2,
+    'avg': (15 + 10) / 2,
   },
   'weight': { # grams
     'min': 100,
@@ -38,18 +38,48 @@ GRAPEFRUIT = {
   },
 }
 
-def main():
+def generate_sorted_values(attribute, configuration, variance_reduction):
+  value_scale = max(
+    configuration[attribute]['max'] - configuration[attribute]['avg'],
+    configuration[attribute]['avg'] - configuration[attribute]['min']
+  ) / variance_reduction
 
+  values = np.random.normal(
+    loc=configuration[attribute]['avg'],
+    scale=value_scale,
+    size=FRUIT_COUNT,
+  )
+  return sorted(values)
 
-  diameter_adjustments = np.random.normal(
-    loc=ORANGE['diameter']['avg'],
+def generate_color_values(color, variance):
+  return np.random.normal(
+    loc=color,
+    scale=variance,
+    size=FRUIT_COUNT,
   )
 
-  for _ in range(FRUIT_COUNT):
-    print("orange,")
+def main():
+  fruit = {
+    'orange': ORANGE,
+    'grapefruit': GRAPEFRUIT
+  }
 
-#  for _ in range(FRUIT_COUNT):
-#    print("grapefruit,")
+  print('name,diameter,weight,red,green,blue')
+  for name, config in fruit.items():
+    diameters = generate_sorted_values('diameter', config, 2)
+    weights = generate_sorted_values('weight', config, 4)
+
+    reds = generate_color_values(config['color']['r'], 6)
+    greens = generate_color_values(config['color']['g'], 10)
+    blues = generate_color_values(config['color']['b'], 6)
+
+    for i in range(FRUIT_COUNT):
+      diameter = round(diameters[i], 2)
+      weight = round(weights[i], 2)
+      red = int(round(max(min(reds[i], 255), 2)))
+      green = int(round(max(min(greens[i], 255), 2)))
+      blue = int(round(max(min(blues[i], 255), 2)))
+      print(f'{name},{diameter},{weight},{red},{green},{blue}')
 
 if __name__ == '__main__':
   main()
