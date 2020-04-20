3dfb1f2b300d0f6286cacc93d44aad6232db31a4
Josh McAdams <joshua.mcadams@gmail.com>
2020-04-20 15:27:24 -0700

    Generated new dataset to use

--- v2/07_deep_learning/00_recurrent_neural_networks/src/data_gen.py
+++ v2/07_deep_learning/00_recurrent_neural_networks/src/data_gen.py
@@ -1,63 +1,14 @@
-"""
-data_gen.py: Generate data for this unit
-"""
-
 import numpy as np
-import random
 
-# Let's first create an array containing the price at the end of the day for our fictional stock for
-# 10 years. Let's assume every day is a trading day so that we don't have to contend with weekends.
-# And let's assume that every year has 365 days, for simplicity.
-#
-# For now, we'll just set the price at each day at zero.
+count = 10000
 
-np.random.seed(1979)
+time = np.linspace(0, count, count)
+series = (
+  np.sin((time - 0.1461656368573156) * (0.9260740013361775 * 10 + 10)) +
+  np.sin((time - 0.3763908388321049) * (0.2301190934334209 * 20 + 20)) +
+  np.sin((time - 0.9602823889173964) * (0.3960271197332794 * 30 + 30)) +
+  0.1 * (np.random.rand(count) - 0.5)
+)
 
-days_per_year = 365
-years = 10
-
-ipos = [5.0, 10.0, 7.5, 12, 15]
-eod_prices = np.zeros((days_per_year * years, len(ipos)))
-
-# We can now set the price at the IPO (day 0).
-
-for i, ipo in enumerate(ipos):
-  eod_prices[0][i] = ipo
-
-# Starting with the IPO, let's fill the remaining prices with random data added to the IPO price with
-# a slight positive bias.
-
-min_bias = 0.0001
-max_bias = 0.0003
-
-for i in range(1, len(eod_prices)):
-  for j in range(len(ipos)):
-    # Find yesterday's price.
-    yesterdays_price = eod_prices[i-1][j]
-
-    # Generate a random a percentage change on the normal curve.
-    percentage_change = np.random.randn(1)[0]
-
-    # The random number is a value on the standard normal distribution
-    # with a mean 0 and variance 1. This will give us a nice range of
-    # positive and negative values, but we need to divide by 100 to scale
-    # them down to reasonable percentages for daily stock price changes.
-    percentage_change /= 100
-
-    # And finally we give the change just a little bit of positive bias
-    # so that we get a nice growth curve.
-    percentage_change += random.uniform(min_bias, max_bias)
-
-    # Calculate the new price.
-    todays_price = yesterdays_price + yesterdays_price * percentage_change
-
-    if todays_price < 0.01:
-      todays_price = random.uniform(0.01, 0.05)
-
-    # Store the price.
-    eod_prices[i][j] = todays_price
-
-print(','.join(['AAA', 'BBB', 'CCC', 'DDD', 'EEE']))
-for prices in eod_prices:
-  print(','.join([f'{price:.2f}' for price in prices]))
-
+for datapoint in series:
+  print(datapoint)
