635897e25e7d03528462e452ad896752b79c05a4
Josh McAdams <joshua.mcadams@gmail.com>
2020-07-27 13:52:46 -0700

    updated images

--- src/tools/images.py
+++ src/tools/images.py
@@ -16,20 +16,21 @@ def find_images(file_name):
       license_pattern = re.compile(r'\s*\*\s*\[([^]]*)\]\(([^)]*)\):\s+(.*)')
       match = re.match(image_pattern, line)
       if match:
-        image_file_name = os.path.split(match.group(2))[-1].strip()
+        #image_file_name = os.path.split(match.group(2))[-1].strip()
         ifn = os.path.join(os.path.split(file_name)[0], match.group(2))
-        ifn = 'https://github.com/google/applied-machine-learning-intensive/tree/master/v2/' + ifn[1:]
-        images[image_file_name] = ifn
+        k = ifn[2:]
+        ifn = 'https://github.com/google/applied-machine-learning-intensive/tree/master/v2/' + ifn[2:]
+        images[k] = ifn
       match = re.match(license_pattern, line)
       if match:
-        image_file_name = os.path.split(match.group(1))[-1].strip()
+        #image_file_name = os.path.split(match.group(1))[-1].strip()
         url = match.group(2).strip()
         license = match.group(3).strip()
         if url == 'https://opensource.google/docs/copyright/':
           license = 'Google (confirmed)'
-        licenses[image_file_name] = {
-            'url': url,
-            'license': license,
+        licenses[k] = {
+          'url': url,
+          'license': license,
         }
       line = f.readline()
 
@@ -49,8 +50,6 @@ def find_images(file_name):
         'name': image,
         'license': license['license']
       }
-  #print(images)
-  #print(licenses)
 
 for (root, dirs, files) in os.walk('.'):
   for f in files:
@@ -69,7 +68,7 @@ for license, count in summary.items():
   print("  * ", license, ": ", str(count))
 print("")
 
-print("## Images") 
+print("## Images")
 for license in summary.keys():
   print('### ', license)
   for image, details in all_images.items():
